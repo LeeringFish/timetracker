eac312878b245f52861b7cac03e35a86d70e011b
LeeringFish <theleeringfish@proton.me>
2025-12-24 16:35:14 -0500

    add DailyRecord class

---
+++ DailyRecord.java
@@ -0,0 +1,24 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class DailyRecord {
+    private LocalDate date;
+    private ArrayList<DailyTask> tasks;
+
+    public DailyRecord(LocalDate date) {
+        this.date = date;
+        this.tasks = new ArrayList<DailyTask>();
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public ArrayList<DailyTask> getTasks() {
+        return tasks;
+    }
+
+    public void add(DailyTask task) {
+        tasks.add(task);
+    }
+}
